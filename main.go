8ea758ee38f5bd1b49b2d9da69abe95a49fe5256
Yernazar <yernazar.uk@gmail.com>
2025-03-21 02:35:19 +0500

    Add routing and server setup with Gorilla Mux

--- main.go
+++ main.go
@@ -3,8 +3,24 @@ package main
 import (
     "log"
     "net/http"
+    "github.com/gorilla/mux"
+    "user-api/config"
+    "user-api/handlers"
 )
 
 func main() {
-    log.Println("Server will start here...")
+    db, err := config.InitDB()
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    r := mux.NewRouter()
+    
+    r.HandleFunc("/users", handlers.CreateUser(db)).Methods("POST")
+    r.HandleFunc("/users/{id}", handlers.GetUser(db)).Methods("GET")
+    r.HandleFunc("/users/{id}", handlers.UpdateUser(db)).Methods("PUT")
+
+    log.Println("Server starting on :8080...")
+    log.Fatal(http.ListenAndServe(":8080", r))
 }
