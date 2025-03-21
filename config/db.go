e28c7498f065ae2bf559d4ff9c3cd4b9907b8c2e
Yernazar <yernazar.uk@gmail.com>
2025-03-21 11:30:44 +0500

    Add retry logic to db connection in config

--- config/db.go
+++ config/db.go
@@ -2,20 +2,34 @@ package config
 
 import (
     "database/sql"
+    "log"
+    "time"
     _ "github.com/lib/pq"
 )
 
 func InitDB() (*sql.DB, error) {
     connStr := "postgres://user:password@localhost:5432/users_db?sslmode=disable"
-    db, err := sql.Open("postgres", connStr)
-    if err != nil {
-        return nil, err
+    var db *sql.DB
+    var err error
+
+    // Повторные попытки подключения
+    for i := 0; i < 10; i++ { // Пробуем 10 раз
+        db, err = sql.Open("postgres", connStr)
+        if err != nil {
+            log.Printf("Failed to open DB: %v", err)
+            time.Sleep(2 * time.Second) // Ждем 2 секунды перед повторной попыткой
+            continue
+        }
+
+        err = db.Ping()
+        if err == nil {
+            log.Println("Successfully connected to database")
+            return db, nil
+        }
+        
+        log.Printf("Failed to ping DB: %v, retrying in 2s...", err)
+        time.Sleep(2 * time.Second)
     }
-    
-    err = db.Ping()
-    if err != nil {
-        return nil, err
-    }
-    
-    return db, nil
+
+    return nil, err // Если после 10 попыток не удалось, возвращаем ошибку
 }
